d2a8b6bd4949959411fa6038ac0cfa363539b2de
uriel1607 <126885696+uriel1607@users.noreply.github.com>
2024-10-23 10:46:27 -0300

    Update informacoes.js

--- informacoes.js
+++ informacoes.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/Akiovictor/trabalho_json_grafico/refs/heads/main/informacoes.json'
+const url = 'https://raw.githubusercontent.com/uriel1607/grafico/refs/heads/main/disciplina.json'
 
 async function vizualizarInformacoes() {
     const res = await fetch(url)
